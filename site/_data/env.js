a8976ae35756021e4746c0acdd811ede175f8126
Silvestar Bistrović <malimirkeccita@gmail.com>
2022-09-18 08:15:10 +0200

    Updating build environments.

--- site/_data/env.js
+++ site/_data/env.js
@@ -1,5 +1,6 @@
 module.exports = {
   production: process.env.ELEVENTY_ENV === 'production',
   development: process.env.ELEVENTY_ENV === 'development',
+  preview: process.env.ELEVENTY_ENV === 'preview',
   env: process.env.ELEVENTY_ENV || 'development'
 };
