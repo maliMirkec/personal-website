8d1894f7b8236a30eaf54073d58b7bec4157632f
Silvestar Bistrović <malimirkeccita@gmail.com>
2023-02-15 19:00:37 +0100

    Testing og image generator.

--- netlify/functions/og-img.js
+++ netlify/functions/og-img.js
@@ -30,6 +30,7 @@ exports.handler = async function (event) {
       isBase64Encoded: true,
     }
   } catch (error) {
+    console.log(error);
     return {
       statusCode: 200,
       body: 'Error!',
