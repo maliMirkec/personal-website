88693b166ddc64816723aaf138d29bd772d906f7
Silvestar Bistrović <malimirkeccita@gmail.com>
2023-02-16 09:26:08 +0100

    Testing og image generator.

--- netlify/functions/og-img.js
+++ netlify/functions/og-img.js
@@ -1,10 +1,15 @@
-const puppeteer = require('puppeteer')
+const chromium = require('chrome-aws-lambda')
 
 exports.handler = async function (event) {
   try {
     const url = event.rawUrl.replace('og-img', 'og-svg')
 
-    const browser = await puppeteer.launch()
+    const browser = await chromium.puppeteer.launch({
+      executablePath: await chromium.executablePath,
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      headless: chromium.headless,
+    })
     const page = await browser.newPage()
     await page.goto(url)
     const buffer = await page.screenshot({
