b5a9f7959dd3ca69f5132fcee4b6ed3ab73d8a0e
Silvestar Bistrović <malimirkeccita@gmail.com>
2023-02-16 07:55:01 +0100

    Testing og image generator.

--- netlify/functions/og-img.js
+++ netlify/functions/og-img.js
@@ -1,38 +1,26 @@
-// const puppeteer = require('puppeteer')
-const { chromium } = require("playwright");
+const puppeteer = require('puppeteer')
 
 exports.handler = async function (event) {
-  const url = event.rawUrl.replace('og-img', 'og-svg')
-
   try {
-    let browser = await chromium.launch();
+    const url = event.rawUrl.replace('og-img', 'og-svg')
 
-    let page = await browser.newPage();
-    await page.goto(url);
-    const buffer = await page.screenshot();
-    console.log(buffer);
-    await browser.close();
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    await page.goto(url)
+    const buffer = await page.screenshot({
+      fullPage: true
+    })
+    await page.close()
+    await browser.close()
 
-    // const browser = await puppeteer.launch()
-    // const page = await browser.newPage()
-    // await page.goto(url)
-    // const buffer = await page.screenshot({
-    //   fullPage: true
-    // })
-    // await page.close()
-    // await browser.close()
-
-
-
-
-    // return {
-    //   statusCode: 200,
-    //   body: buffer.toString('base64'),
-    //   headers: {
-    //     "content-type": "image/jpeg"
-    //   },
-    //   isBase64Encoded: true,
-    // }
+    return {
+      statusCode: 200,
+      body: buffer.toString('base64'),
+      headers: {
+        "content-type": "image/jpeg"
+      },
+      isBase64Encoded: true,
+    }
   } catch (error) {
     console.log(error);
     return {
