46229121581a62462bc97ad14ab89bd96bde4b0b
Silvestar Bistrović <malimirkeccita@gmail.com>
2023-03-23 11:13:43 +0100

    Preparing a11y testing.

--- site/_data/env.js
+++ site/_data/env.js
@@ -3,5 +3,6 @@ module.exports = {
   development: process.env.ELEVENTY_ENV === 'development',
   staging: process.env.ELEVENTY_ENV === 'staging',
   preview: process.env.ELEVENTY_ENV === 'preview',
+  a11y: process.env.ELEVENTY_ENV === 'a11y',
   env: process.env.ELEVENTY_ENV || 'development'
 };
